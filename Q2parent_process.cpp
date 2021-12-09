c6a7f314ab7c85b81e2187e898a7220c18db2e95
Ujjawal kumar <kumarujjawal1408@gmail.com>
2021-12-09 11:14:21 +0530

    Add files via upload

---
+++ Q2parent_process.cpp
@@ -0,0 +1,31 @@
+//Set-3 OS Practical Exam
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+using namespace std;
+
+int n=10;
+int main()
+{
+	cout<<"original value of n= "<<n<<endl;
+	pid_t pid;
+	pid=fork();
+
+	if(pid==0)      //child process
+	{
+		cout<<"child process:"<<endl;
+		n= n-5;
+		cout<<"n = "<<n<<endl;
+		return 0;
+	}
+
+	else if(pid>0)		//parent process
+	{
+		cout<<"parent process:"<<endl;
+		n=n+5;
+		cout<<"n = "<<n<<endl;
+		return 0;
+	}
+
+}
+
